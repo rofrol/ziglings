c752c02a0c2467eb4755215a0276e46d9a2ff757
Roman Frołow <rofrol@gmail.com>
2021-03-31 02:57:10 +0200

    wip

--- exercises/048_methods2.zig
+++ exercises/048_methods2.zig
@@ -54,7 +54,7 @@ fn visitElephants(first_elephant: *Elephant) void {
 
         // Get the next elephant or stop.
         if (e.hasTail()) {
-            e = e.???; // Which method do we want here?
+            e = e.getTail(); // Which method do we want here?
         } else {
             break;
         }
